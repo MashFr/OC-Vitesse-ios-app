bd0df1cedb7526f31d630b56dc1e84d7a94dc7da
MashFr <ecouvillon_llanos0z@icloud.com>
2024-11-05 19:53:53 +0100

    Initial Commit

---
+++ Vitesse/VitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseApp.swift
+//  Vitesse
+//
+//  Created by Tony Stark on 05/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct VitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
